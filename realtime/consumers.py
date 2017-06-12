e99cb9d5987b712618cf951039711a2ac715f3c0
mileshobby <mhobby16@gmail.com>
2017-06-11 19:29:46 -0700

    update  wss ready alert to have type ready

--- realtime/consumers.py
+++ realtime/consumers.py
@@ -20,7 +20,7 @@ def ws_add(message):
         if connection.users.count() == 1:
             connection.users.add(message.user.pk)
             Group(str(connection.pk)).add(message.reply_channel)
-            message.reply_channel.send({'ready': True})
+            message.reply_channel.send(json.dumps({'disconnected': 'true'}))
             return
 
     #if no open chats
